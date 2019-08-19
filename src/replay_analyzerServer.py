453f2429eb7d066c8a39d7692974472cc076b89a
jiangminghao <jiangminghao@antiy.cn>
2019-08-19 18:55:18 +0800

    fix bugs.

--- src/replay_analyzerServer.py
+++ src/replay_analyzerServer.py
@@ -147,8 +147,10 @@ def getHandler(args):
             # give flexibility in setting parameters
             print(type(db))
             print(userID,historyCount,testID)
-
-            response = db.getSingleResult(userID, historyCount, testID)
+            if type(db) == DB:
+                response = db.getSingleResult(userID, historyCount)
+            else:
+                response = db.getSingleResult(userID, historyCount, testID)
             # response = processResult(response)
             # Should only be one result since unique (userID, historyCount, testID)
             return json.dumps({'success':True, 'response':response[0]}, cls=myJsonEncoder)
